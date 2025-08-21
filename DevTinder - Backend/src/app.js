b7fa639143c35fddf4cd4f8011357dd90fd693bf
Prince Mishra <princemishra95927@gmail.com>
2025-08-21 23:00:58 +0530

    Understanding of Postman

--- DevTinder - Backend/src/app.js
+++ DevTinder - Backend/src/app.js
@@ -2,19 +2,36 @@ const express = require("express")
 
 const app = express();
 
+app.use("/user", (req, res) => {
+    res.send("hahahaha")
+})
+
+// * This will only handle get to /user
+app.get("/user", (req, res) => {
+    res.send(
+        {
+            firstName: "Prince",
+            lastName: "Mishra"
+        }
+    )
+})
+
+
+app.post("/user", (req, res) => {
+    //* Saving data to db
+    res.send("Data successfully saved to the database")
+})
+
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted Successfully")
+})
 
 
 
+//! this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("hello from the server")
-})
-
-app.use("/hello", (req, res) => {
-    res.send("hello hello hello hello ")
-})
-
-app.use("/", (req, res) => {
-    res.send("Welcome")
 })
 
 
