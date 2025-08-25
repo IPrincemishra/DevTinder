60fe4477837e94e335d8022881a0ba10f6c3a7b7
Prince Mishra <princemishra95927@gmail.com>
2025-08-25 08:04:46 +0530

    Passing & Getting data from database

--- DevTinder - Backend/src/app.js
+++ DevTinder - Backend/src/app.js
@@ -3,15 +3,13 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
 
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
+    // console.log(req.body);
+
     // * Creating a new instance of the User model
-    const user = new User({
-        firstName: "Virat",
-        lastName: "Kholi",
-        emailId: "Virat@google.com",
-        password: "virat$12345"
-    });
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User added Successfully")
@@ -20,7 +18,43 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+//* Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId
 
+    try {
+        const user = await User.findOne({ emailId: userEmail })
+        if (!user) {
+            res.status(404).send("user not found")
+        }else{
+            res.send(user)
+        }
+        // const users = await User.find({
+        //     emailId: userEmail
+        // })
+
+        // if (users.length === 0) {
+        //     res.status(404).send("user not found")
+        // } else {
+        //     res.send(users)
+        // }
+    } catch (err) {
+        res.status(400).send("Somthing went wrong")
+    }
+})
+
+//! feed API - To get all the users from the database 
+app.get("/feed", async (req, res) => {
+    // console.log(User.name);
+
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (err) {
+        res.status(404).send("Something went wrong")
+    }
+
+})
 
 connectDB()
     .then(() => {
